4d3cedc46d92c518b6cb1bb3a7bdc7b84b1d34d5
Augustno Anthony Mageka <augustinomageka@gmail.com>
2023-04-19 14:48:41 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/tafakari/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tafakari
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:tafakari/screens/tafakariNasi/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tafakari Nasi',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: TafakariHomeScreen.routeName,
+      routes: {
+        TafakariHomeScreen.routeName: (context) => TafakariHomeScreen(),
+      },
+    );
+  }
+}
